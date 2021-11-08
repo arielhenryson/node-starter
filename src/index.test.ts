e0c92d6b4297f68611490222b5f4780f84206c31
Ariel Henryson <ariel@codefresh.io>
2021-11-08 14:47:05 +0200

    work

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,13 @@
+import got from 'got'
+
 describe('Test Description', () => {
   it('TestName', () => {
     expect(1).toBe(1)
+  })
+
+  it('test main endpoint', async () => {
+    const res: any = await got(`http://localhost:3000/`).json()
+
+    expect(res.foo).toEqual('bar')
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import cors = require('cors')
 import express = require('express')
-import { Request, Response } from 'express'
 
 const app = express()
 app.use(cors())
 const port = 3000
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello world')
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send({
+    foo: 'bar'
+  })
 })
 
 app.listen(port, () => {
